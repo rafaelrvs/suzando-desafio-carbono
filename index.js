eced3e137b317276c2f894c26f8864969c730a4f
rafaelrvs <rafaelvieira1333@gmail.com>
2023-10-03 17:13:21 -0300

    adote ja

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Seletor do botão
-const whatsappButton = document.querySelector('#whatsappButton');
+const whatsappButton = document.querySelector('.whatsappButton');
 
 // Número de telefone para o qual você deseja criar o link do WhatsApp
 const numeroTelefone = '5511945932318';
@@ -7,7 +7,6 @@ const numeroTelefone = '5511945932318';
 // Manipulador de evento de clique do botão
 whatsappButton.addEventListener('click',function(){
     // Crie o URL do WhatsApp com o número de telefone
-    console.log('test')
     const url = `https://api.whatsapp.com/send?phone=${numeroTelefone}`;
     // Redirecione o usuário para o WhatsApp
     window.location.href = url;
